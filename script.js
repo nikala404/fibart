e65f8ba3c82db5affa80b093fe50f986eb38d9c7
nikala <nikoloz.lursmanashvili.1@btu.edu.ge>
2024-06-15 14:15:08 +0400

    fix header

--- script.js
+++ script.js
@@ -3,9 +3,16 @@ const navUl = document.querySelector("nav");
 const navLinks = document.querySelectorAll("nav ul li a");
 const logo = document.querySelector(".logo");
 const learnMore = document.querySelector(".learn-more");
+const heroSection = document.querySelector(".hero");
+const header = document.querySelector(".header_content");
 
 menuIcon.addEventListener("click", () => {
   navUl.classList.toggle("active");
+  if (navUl.classList.contains("active")) {
+    navUl.style.width = "100%";
+  } else {
+    navUl.style.width = "auto";
+  }
 });
 
 navLinks.forEach((link) => {
@@ -18,6 +25,19 @@ logo.addEventListener("click", () => {
   window.location.href = "index.html";
 });
 
-learnMore.addEventListener("click", () => {
-  window.moveTo(1450, 0);
+const scrollToSection = (section) => {
+  window.scrollTo({
+    top: section.offsetTop,
+    behavior: "smooth",
+  });
+};
+
+window.addEventListener("scroll", function () {
+  const heroHeight = heroSection.offsetHeight;
+
+  if (window.scrollY > heroHeight) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
 });
